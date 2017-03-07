aa462bb47d5503aa3642f10b6c7e99501f48ee75
Lee-PC\Lee <leegunj@sqisoft.com>
2017-03-07 14:32:22 +0900

    2

--- src/main/java/com/ktds/board/articles/biz/ArticlesBiz.java
+++ src/main/java/com/ktds/board/articles/biz/ArticlesBiz.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.ktds.board.articles.vo.ArticlesVO;
 
-public interface ArticlesBiz {
+public interface ArticlesBiz { 
 	public List<ArticlesVO> getAllArticles();
 	public boolean writeArticles(ArticlesVO article);
 	public ArticlesVO getArticleBy(String articlesId);
